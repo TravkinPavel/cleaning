8e5a6b8a422a13301d31df874a0c8e24b73ac91a
DESKTOP-KQ4OP4B\Pavel <pavelstravkins@gmail.com>
2017-02-27 23:53:07 +0200

    'test1'

--- js/common.js
+++ js/common.js
@@ -1,3 +1,151 @@
 $(document).ready(function() {
-
+$('.owl-carousel-1').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1
+        },
+         480:{
+            items:2
+        },
+        768:{
+            items:4
+        },
+        992:{
+            items:6
+        }
+    }
+});
+$('.owl-carousel-2').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:2
+        }
+    }
+});
+                //E-mail Ajax Send
+    $("#form1").submit(function() { //Change
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "mail.php", //Change
+            data: th.serialize()
+        }).done(function() {
+            $('.block__thank_you').addClass('block__thank_you_active')
+            setTimeout(function() {
+                // Done Functions
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
+    });
+                    //E-mail Ajax Send
+    $("#form2").submit(function() { //Change
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "mail.php", //Change
+            data: th.serialize()
+        }).done(function() {
+            $('.block__thank_you').addClass('block__thank_you_active')
+            setTimeout(function() {
+                // Done Functions
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
+    });
+                    //E-mail Ajax Send
+    $("#form3").submit(function() { //Change
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "mail.php", //Change
+            data: th.serialize()
+        }).done(function() {
+            $('.block__thank_you').addClass('block__thank_you_active')
+            setTimeout(function() {
+                // Done Functions
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
+    });
+                    //E-mail Ajax Send
+    $("#form4").submit(function() { //Change
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "mail.php", //Change
+            data: th.serialize()
+        }).done(function() {
+            $('.block__thank_you').addClass('block__thank_you_active')
+            setTimeout(function() {
+                // Done Functions
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
+    });
+                    //E-mail Ajax Send
+    $("#form5").submit(function() { //Change
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "mail.php", //Change
+            data: th.serialize()
+        }).done(function() {
+            $('.block__thank_you').addClass('block__thank_you_active')
+            setTimeout(function() {
+                // Done Functions
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
+    });
+                    //E-mail Ajax Send
+    $("#form6").submit(function() { //Change
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "mail.php", //Change
+            data: th.serialize()
+        }).done(function() {
+            $('.block__thank_you').addClass('block__thank_you_active')
+            setTimeout(function() {
+                // Done Functions
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
+    });
+                    //E-mail Ajax Send
+    $("#form7").submit(function() { //Change
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "mail.php", //Change
+            data: th.serialize()
+        }).done(function() {
+            $('.block__thank_you').addClass('block__thank_you_active')
+            setTimeout(function() {
+                // Done Functions
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
+    });
 }); 
